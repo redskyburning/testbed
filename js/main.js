f18d44b4a39888ff49a5a7a64807b49bcc3d31af
Ben Webb <ben@redskyburning.com>
2012-08-26 23:21:33 -0400

    Mobile P v1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+function vInfo(){
+	var vw = $(window).width();
+	var vh = $(window).height();
+	vm = (vw > vh) ? vh : vw;
+	var ratio = (vw < vh) ? vh / vw : vw /vh;
+	var arr = [vh,vw,vm,Math.round(ratio*100)/100];
+	return arr;
+}
 
+function vWatcher(){
+	$('#viewportWatch').html(vInfo().join(', '));
+	$('body').css('font-size',(vm * .03) + 'px');
+}
+
+$(document).ready(function(){
+	vWatcher();
+});
+
+$(window).resize(function() {
+	vWatcher();
+});
