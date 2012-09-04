804a4f2401236414960f24aac989dd9ebf0ead18
Ben Webb <ben@redskyburning.com>
2012-09-03 20:13:32 -0400

    functional article <woot!>

---
+++ js/v2.js
@@ -0,0 +1,33 @@
+function vInfo(){
+	var vw = $(window).width();
+	var vh = $(window).height();
+	vm = (vw > vh) ? vh : vw;
+	var ratio = (vw < vh) ? vh / vw : vw /vh;
+	var arr = [vh,vw,vm,Math.round(ratio*100)/100];
+	return arr;
+}
+
+function vWatcher(){
+	$('#viewportWatch').html(vInfo().join(', '));
+	$('body').css('font-size',(vm * .01) + 'px');
+}
+
+function toggleNav(){
+	var t = $('#siteNav');
+	if(t.hasClass('active')){
+		t.removeClass('active');
+	} else {
+		t.addClass('active');
+	}
+}
+
+$(document).ready(function(){
+	vWatcher();
+	$('#navToggle').click(function(){
+		$('#siteNav').toggle();
+	});
+});
+
+$(window).resize(function() {
+	vWatcher();
+});
